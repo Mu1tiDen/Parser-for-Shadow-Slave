172c8cce888aacd61ccdaf77ce4f73d31c7d86d7
mu1tiden <multiden007@gmail.com>
2025-08-13 22:08:24 +0300

    add docx support

--- src/parser.py
+++ src/parser.py
@@ -1,54 +1,77 @@
 import requests
-#from docx import Document
+import os
+from docx import Document
 from bs4 import BeautifulSoup
-#import os
 
-def find_next_link(soup):
-    links = soup.find_all("a", href=True)
+
+
+def find_next_link(soup_):
+    links = soup_.find_all("a", href=True)
+
     for link in links:
         if link.text in "Следующая глава":
             with open("data/link.txt", 'w') as file:
                 file.write(f'https://telegra.ph{link.get('href')}')
                 #https://telegra.ph/Glava-2324-Hod-protivnika-05-13
             print("New link successfully write!")
-            return "NOT_NONE_LOL"
-    return "NONE"
+            return False
+
+    return True
+
+
 def put_chapter_in_file():
+    if not os.path.exists("data/link.txt"):
+        with open("data/link.txt", 'w') as file:
+            file.write("https://telegra.ph/Glava-989-Padenie-Falkon-Skotta-7-06-23")
+
     with open("data/link.txt") as file:
         url = file.read()
 
     headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept": "*/*",
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0"
     }
+
     req = requests.get(url, headers=headers)
     src = req.text
 
     with open("data/index.html", "w") as file:
         file.write(src)
-    with open("data/index.html") as file:
-        src = file.read()
 
     soup = BeautifulSoup(src, "lxml")
 
-    chap_title = soup.find(class_="tl_article_content").find("h1")
-    chap_text = soup.find(class_="tl_article_content").find_all("p")
+    chapter_title = soup.find(class_="tl_article_content").find("h1")
+    chapter_text = soup.find(class_="tl_article_content").find_all("p")
 
     with open("data/test.txt", "a") as file:
-        file.write(f'{chap_title.text.upper()}\n')
-        for text_line in chap_text:
+        file.write(f'{chapter_title.text.upper()}\n')
+        for text_line in chapter_text:
             filtered_line = text_line.text
             if filtered_line != '\n' and filtered_line not in "Предыдущая глава Следующая глава":
                 file.write(f'{filtered_line}\n')
         file.write('\n')
-        print(chap_title.text.upper(), ' recorded successfully!\n')
+
+        print(f'"{chapter_title.text}" recorded successfully!\n')
+
+    if os.path.exists("data/test.docx"): document = Document("data/test.docx")
+    else: document = Document()
+
+    title = document.add_heading(chapter_title.text, 1)
+    title.bold = True
+
+    for text_line in chapter_text:
+        filtered_line = text_line.text
+        if filtered_line != '\n' and filtered_line not in "Предыдущая глава Следующая глава":
+            document.add_paragraph(filtered_line, style="No Spacing")
+
+    document.save("data/test.docx")
+
+
     return soup
 
 while True:
     soup = put_chapter_in_file()
-    if find_next_link(soup) == "NONE":
+    if find_next_link(soup):
         print('popa')
         break
-
-
 
